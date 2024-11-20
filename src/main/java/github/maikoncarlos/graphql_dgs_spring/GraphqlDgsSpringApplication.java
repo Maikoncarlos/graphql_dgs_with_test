de59760d809fc0e4d22c3495e6778e849547d97e
Maikoncarlos <maikonsaturno@gmail.com>
2024-11-20 19:22:50 -0300

    configuration property application

---
+++ src/main/java/github/maikoncarlos/graphql_dgs_spring/GraphqlDgsSpringApplication.java
@@ -0,0 +1,13 @@
+package github.maikoncarlos.graphql_dgs_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlDgsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlDgsSpringApplication.class, args);
+	}
+
+}
